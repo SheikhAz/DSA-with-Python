0defe5c3ba435fe09ce390a38dfea7580dd10d90
Shiekh Azmatulla <shiekazmatulla@gmail.com>
2026-05-04 10:36:19 +0530

    Factorial

---
+++ Recursion/functionalRecursion.py
@@ -0,0 +1,30 @@
+
+# * Sum Using Recursion
+
+# def funR(N):
+#     if N==1:
+#         return 1
+#     return N + funR(N-1)
+
+# x = funR(10)
+# print(x)
+
+# * Factorial Using Recursion
+
+# def funR(N):
+#     if N==1:
+#         return 1
+#     return N * funR(N-1)
+
+# x = funR(5)
+# print(x)
+
+# * Factorial Using Loop
+
+
+# fact = 1
+# n = 5
+# for i in range(1,n+1):
+#     fact = fact * i
+
+# print(fact)
